731fd75b09d537f3dccf5aff5fb2a4fc5ee09b78
Coby Tao <cobytao@hotmail.com>
2016-08-04 22:56:35 +0000

    deploy bugfix

--- server.js
+++ server.js
@@ -2,11 +2,22 @@
 
 var express = require('express');
 var multer = require('multer');
+var path = require('path');
 var app = express();
 var upload = multer();
 
 app.get('/', function(req, res) {
-    res.end("Test");
+    app.get('/', function(req, res) {
+        var guidePath = path.join(__dirname, "index.html");
+        res.sendFile(guidePath, function (err) {
+            if (err) {
+                throw err;
+            }
+            else {
+                console.log("Guide sent");
+            }
+        });
+    });
 });
 
 app.post('/size', upload.single('file'), function (req, res) {
