634c9b94b7050f440dd9b7242c8fece2158abbf4
Coby Tao <cobytao@hotmail.com>
2016-08-04 22:54:35 +0000

    deploy bugfix

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var multer = require('multer');
 var app = express();
 var upload = multer();
 
-app.get('/', express.static("views"));
+app.get('/', function(req, res) {
+    res.end("Test");
+});
 
 app.post('/size', upload.single('file'), function (req, res) {
    res.json({
